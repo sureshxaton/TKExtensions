98606f1f2a8c100ef2d06071d1eb47e661adceac
Taras Kalapun <t.kalapun@gmail.com>
2014-07-24 09:50:50 +0200

    Update SVProgressHUD+TKExtensions.h

--- SVProgressHUD+TKExtensions.h
+++ SVProgressHUD+TKExtensions.h
@@ -1,6 +1,8 @@
+#ifdef COCOAPODS_POD_AVAILABLE_SVProgressHUD
 #import "SVProgressHUD.h"
 @interface SVProgressHUD (TKExtensions)
 + (void)showBlocking;
 + (void)showError:(NSError *)error;
 + (void)dismissWithPossibleMessage:(id)responce;
 @end
+#endif
